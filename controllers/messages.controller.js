2ad0fd256e7ad9a3e3f1e024857d301adab9f68b
michaloumen <michelle.mendonca@minu.co>
2023-02-03 22:45:58 -0300

    adding handlebars

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 function getMessages(req, res) {
-  res.sendFile(path.join(__dirname, "..", 'public', 'Example.jpg'));
+  res.sendFile(path.join(__dirname, "..", 'public', 'images', 'Example.jpg'));
 }
 
 function postMessage(req, res) {
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const friendsRouter = require('./routes/friends.router');
 const messagesRouter = require('./routes/messages.router');
 
 const app = express();
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 const PORT = 3000;
 
 app.use((req, res, next) => {
@@ -12,8 +15,15 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
+app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'My Friends',
+    caption: 'Image example'
+  })
+});
 app.use('/friends', friendsRouter);
 app.use('/messages', messagesRouter);
 
